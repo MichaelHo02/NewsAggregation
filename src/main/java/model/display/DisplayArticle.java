92fbdda7103ba942c993b8c47a3fd3f8bf582809
Tommy <s3878174@rmit.edu.vn>
2021-08-24 22:27:00 +0700

    Add zingnews and Tuoi tre + scrapping Engine

---
+++ src/main/java/model/display/DisplayArticle.java
@@ -0,0 +1,21 @@
+package model.display;
+
+import com.sun.javafx.iio.ImageLoader;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import model.scrapping_engine.Article;
+import org.jsoup.nodes.Document;
+
+public abstract class DisplayArticle {
+    //TODO:Implement a way to interact with webview
+    private String tmpTemplate;
+    public abstract String articleScraper(Article article);
+
+    public String getTmpTemplate() {
+        return tmpTemplate;
+    }
+
+    public void setTmpTemplate(String tmpTemplate) {
+        this.tmpTemplate = tmpTemplate;
+    }
+}
--- src/main/java/model/display/DisplayZingNews.java
+++ src/main/java/model/display/DisplayZingNews.java
@@ -1,16 +1,10 @@
 package model.display;
 
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 import model.scrapping_engine.Article;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class DisplayTuoiTre extends DisplayArticle{
-        //Return the String of HTML of the article
+public class DisplayZingNews extends DisplayArticle {
     @Override
     public String articleScraper(Article article) {
         try {
@@ -20,17 +14,14 @@ public class DisplayTuoiTre extends DisplayArticle{
                     doc.getElementsByTag("h1").select("*").toString() +
                     "</head>\n" +
                     "<body>\n"  +
-                    "<h1 style = \"font-size:38px;font-weight:bold;\">\n" +
-                    doc.select("h1.article-title").text() +
+                    "<h1 style = \"font-size:38px;\">\n" +
+                    doc.select("ul.the-article-meta").text() +
                     "</h1>\n" +
-                    "<h2> \n" +
-                    doc.select("h2.sapo").text() +
+                    "<h2 style = \"font-size:20px;font-weight : bold;\">\n" +
+                    doc.select("p.the-article-summary").text() +
                     "</h2>\n" +
                     "<p>\n" +
-                    doc.getElementsByClass("column-first-second").select("a,p").text() +
-                    "</p>\n" +
-                    "<p>\n" +
-                    doc.select("div.author").text() +
+                    doc.getElementsByClass("the-article-body").select("td,p").text() +
                     "</p>\n" +
                     "</body>\n" +
                     "</html>";
@@ -39,5 +30,4 @@ public class DisplayTuoiTre extends DisplayArticle{
             return  "<html>" +"<head>" + "Article Scrapping Error" + "</head>\n" + "</html>";
         }
     }
-
 }
