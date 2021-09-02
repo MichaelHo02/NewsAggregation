ee1d252b2e81e504ec13eae5d17ba76e781a17c5
Tommy <s3878174@rmit.edu.vn>
2021-09-02 19:23:26 +0700

    implement Filtering

--- src/main/java/model/filter/ArticleFilter.java
+++ src/main/java/model/filter/ArticleFilter.java
@@ -2,28 +2,46 @@ package model.filter;
 
 import model.get_article_behavior.Article;
 
-public class ArticleFilter {
-    private static Article article;
-    
-    
-    public static void getCategory(Article article) {
-        
-    }
-    
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public  class ArticleFilter {
     //Load prdefined world from file -> Store on file for future upgradability,
-    private String  loadDictionary() {
-        String dictionary = null;
-        return dictionary;
+
+    public static String []  loadDictionary(String dictionaryFile) {
+        String [] dictionary;
+        //Create a scnner to read file
+        try {
+        //Need to read the dictionary in
+            File input = new File(dictionaryFile);
+            String delim = ",";
+            String src = Files.readString(Path.of(input.getPath()));
+            //Need to split the string contain delim
+            dictionary = src.split(delim);
+            return dictionary;
+        } catch (Exception e) {
+            System.out.println("Error with " + dictionaryFile);
+            return null;
+        }
     }
     
     //Implement ís category to get the category
-    private void isCovid() {
-        
+    //TODO: Need 8more methode and define dictionary like thiss
+    public void isWorld(Article article){
+        String [] dict = loadDictionary("dictionary/World");
+        for (String tmp: dict) {
+            if(article.getCategory().matches(tmp)) {
+            //Add to category
+            }
+        }
     }
-    
+
     //Filter action
-    private static void filteringCategory(Article article) {
-        
+    //Call all of the is category
+    public static void  filteringCategory(Article article) {
+        //Call all of other methid
     }
     
     
