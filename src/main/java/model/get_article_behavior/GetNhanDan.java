51de74ff3bf7449a6a884efc7b0cfb2a26df98b0
truongnhatanh17 <s3878231@rmit.edu.vn>
2021-09-05 10:49:45 +0700

    Fix nhandan scraping

--- src/main/java/model/get_article_behavior/GetNhanDan.java
+++ src/main/java/model/get_article_behavior/GetNhanDan.java
@@ -1,14 +1,15 @@
 package model.get_article_behavior;
 
-import com.github.sisyphsu.dateparser.DateParserUtils;
 import model.scrapping_engine.InitScraper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class GetNhanDan extends GetArticleBehavior implements Runnable {
 
@@ -20,43 +21,48 @@ public class GetNhanDan extends GetArticleBehavior implements Runnable {
 
     @Override
     public void scrapeArticle(String url, CopyOnWriteArrayList<Article> articles) {
-
         try {
-            Document doc = Jsoup.connect(url).get();
-            for (Element element : doc.select("div.box-title > a[href]")) { // Fetch all links
-                String tempLink = "https://nhandan.vn" + element.attr("href"); // Join links
-                System.out.println(tempLink);
-                Document tempDoc = Jsoup.connect(tempLink).get(); // Request to the destination link and extract contents
-                String title = tempDoc.select(".box-title-detail.entry-title").text();
-                String date = tempDoc.select(".box-date.pull-left").text();
-                String imageURL = tempDoc.select("img").attr("data-src");
-                String category = tempDoc.select(".uk-breadcrumb a").text();
-                if (title.equals("") || title.isBlank() || title.isEmpty()) { // Handle unpassable article
-                    continue;
-                }
-                else if (date.equals("") || date.isBlank() || date.isEmpty()) { // Handle abnormal date format
-                    continue;
-                }
-                if (!(imageURL.contains("jpg") || imageURL.contains("JPG") || imageURL.contains("jpeg") || imageURL.contains("png"))) {
-                    imageURL = ""; // Prevent bugs with ImageView
-                }
-                Article article = new Article(title, tempLink, DateParserUtils.parseDate(date.substring(9)), imageURL, WebsiteURL.NHANDAN, category);
-                synchronized(this) {
-                    System.out.println(InitScraper.articles);
-                    articles.add(article);
-                }
+            ExecutorService executorService = Executors.newCachedThreadPool();
+            System.out.println(url);
+            Document doc = Jsoup.connect(url).timeout(10000).get();
+            for (Element element : doc.select("article")) { // Fetch all links
+                executorService.submit(() -> {
+                    try {
+                        String tempLink = ""; // Join links
+                        String title = element.getElementsByClass("box-title").text();
+                        String date = element.select("div[class*=box-meta]").text();
+                        String imageURL = element.select("img").attr("data-src");
+                        String category = "";
+                        Date tempDate = new SimpleDateFormat("HH:mm dd/MM/yyyy").parse(date);
+                        Article article = new Article(title, tempLink, tempDate, imageURL, WebsiteURL.NHANDAN, category);
+                        addArticle(article);
+                    } catch (Exception e) {
+                        System.out.println("Cannot parse date");
+                    }
+
+                });
+            }
+            executorService.shutdown();
+            while (!executorService.isTerminated()) {
+//                System.out.println("Nhandan is scraping...");
             }
         } catch (Exception e) {
             System.out.println("Failed to connect");
         }
-
-
     }
 
     @Override
     public void run() {
         scrapeArticle(this.url, InitScraper.articles);
     }
+
+    synchronized void addArticle(Article article) {
+        if (InitScraper.articles.size() == 200) {
+            return;
+        }
+        InitScraper.articles.add(article);
+    }
+
 
 
 }
