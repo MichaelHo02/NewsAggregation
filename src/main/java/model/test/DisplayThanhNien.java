e618532f5c18d996a652e206bd36ad92aef03b15
Tommy <s3878174@rmit.edu.vn>
2021-09-06 23:15:48 +0700

    Finishing display Thanh Nien

--- src/main/java/model/test/DisplayThanhNien.java
+++ src/main/java/model/test/DisplayThanhNien.java
@@ -1,5 +1,8 @@
 package model.test;
 
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.FlowPane;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -16,24 +19,37 @@ public class DisplayThanhNien extends JsoupArticleDisplay {
             Document doc = Jsoup.connect(linkPage).get();
 //          // TODO: Scrap sapo
             String firstImage = doc.select("#contentAvatar img").attr("src");
-            Elements elements = doc.getElementsByClass("cms-body detail").select("*");
+            Elements body = doc.select("div[class~=.*content]");
+            Elements elements = doc.select("div[id=abody] > *");
+//            System.out.println(doc.select("div[id=abody] > *").toString());
+            if (body.select("div.sapo").size() > 0) {
+                Content cont = new Content(body.select("div.sapo").text(),"p");
+                CONTENT.add(cont);
+            }
+            else {
+                Content cont = new Content(doc.select("div.summary").text(),"p");
+                CONTENT.add(cont);
+            }
 
             for (Element element : elements) {
                 if (element.tagName().equals("img")) {
-                    Content tempImg = new Content(element.attr("src"),"img");
+                    Content tempImg = new Content(element.select("div[id=contentAvatar] img").attr("src"),"img");
                     CONTENT.add(tempImg);
                 } else if (element.tagName().equals("p")) {
                     Content tempP = new Content(element.text(), "p");
                     CONTENT.add(tempP);
-                } else if (element.hasClass("clearfix cms-video")) {
-                    Content tempVideo = new Content(element.attr("data-video-src"), "video");
-                    System.out.println(tempVideo.getContext());
                 } else if (element.tagName().matches("h\\d")) {
                     Content tempH = new Content(element.text(), "h");
                     CONTENT.add(tempH);
                     System.out.println(tempH.getContext());
+                } else  if (element.is("div") && !element.className().equals("details__morenews")){
+                    checkDivTN(element);
                 }
             }
+
+            //Add author
+            Content cont= new Content(doc.select("div.left h4").text(),"p");
+            CONTENT.add(cont);
 
         } catch (Exception e) {
             System.out.println("Cannot connect to the page from DisplayTuoiTre");
@@ -41,10 +57,51 @@ public class DisplayThanhNien extends JsoupArticleDisplay {
         return CONTENT;
     }
 
-    public static void main(String[] args) throws Exception {
-        DisplayThanhNien test = new DisplayThanhNien();
-//        DisplayNhanDan test2 = new DisplayNhanDan();
-        test.getContent("https://thanhnien.vn/thoi-su/tphcm-dat-muc-tieu-kiem-soat-dich-covid-19-o-7-quan-huyen-vao-cuoi-thang-8-1430885.html");
-//        System.out.println(test2.getContent("https://nhandan.vn/chuyen-lam-an/gia-van-tai-bien-tang-phi-ma-doanh-nghiep-xuat-khau-lao-dao-659336/").get(0).getContext());
+    private void checkDivTN(Element div) {
+        // If element has 0 children and is not an ad div
+        if (div.select("> *").size() == 0 && !div.className().contains("ads") && div.hasText()){
+            Content tmpdiv = new Content(div.text(),"div");
+            CONTENT.add(tmpdiv);
+            return;
+        }
+
+        // Loop through div elements
+        for (Element i : div.select("> *")) {
+            try {
+                // Recursion call if child is div
+                if (i.is("div") && !i.attr("class").contains("image")) {
+                    checkDivTN(i);
+                }
+                else if (i.is("p")) {
+                    Content ptmp = new Content(i.text(),"p");
+                } else if (i.attr("class").contains("image")) {
+                    Content tmpimg = new Content(i.select("img").attr("data-src"), "img");
+                    Content labimg = new Content(i.select("p").text(),"p");
+                    CONTENT.add(tmpimg);
+                    CONTENT.add(labimg);
+                } else if (i.is("figure") && i.attr("class").equals("picture")) {
+                    if (i.select("img").size() > 0) {
+                      Content cont = new Content(i.select("img").attr("data-src"), "img");
+                      Content lab = new Content(i.select("figcaption").text(),"p");
+                      CONTENT.add(cont);
+                      CONTENT.add(lab);
+                    }
+                    else if (i.hasText()) {
+                        Content txt =new Content(i.text(),"p");
+                    }
+                }
+                // Add header if child is header
+                else if (i.is("h2") || i.is("h3")) {
+                    Content cont = new Content(i.text(), "h");
+                }
+                // Add text label if child is neither image nor video and has text
+                else if (i.hasText()) {
+                   Content cont = new Content(div.text(),"div");
+                    break;
+                }
+            }
+            catch (IllegalArgumentException ex) { continue; }
+        }
     }
+
 }
